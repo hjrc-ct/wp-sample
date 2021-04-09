60aad4ff362261072e73a1d8dd42fa3f6d13ef01
Raghavendra Chari <56157111+hjrc-ct@users.noreply.github.com>
2021-04-09 23:17:31 +0530

    done

--- push.js
+++ push.js
@@ -13,7 +13,14 @@ let vapidKeys =
 push.setVapidDetails('mailto:security@ct.com', 
                         vapidKeys.publicKey, vapidKeys.privateKey);
 
-let sub = {};
+let sub = {
+    "endpoint":"https://fcm.googleapis.com/fcm/send/d6EhN9o_ykY:APA91bHuZnseIjK8N_6LBNFF3J_Lgh3wGrgiyy19Ydy49bwQpN9SqjBdbJ00xQ8NIgp7sZop1qIc-pJc27JMOhtgJoekkQJ344afgjNAP9YBbc2WmZdLvh1UGcLU2HOyo8rNJ1c0fkRs",
+    "expirationTime":null,
+    "keys":{
+        "p256dh":"BMuo1ocprsO8cvdl3fc_Pt2yd7cUptBNdSoL-8_FLC6qBcDpd05kVCygM3scjWdXYLp0bqxpUYHabJaaabJBEEU",
+        "auth":"TsxAJ3Mkjbv3wDNEYByqzA"
+    }
+};
 
 push.sendNotification(sub, 'BPA Message One');
 
