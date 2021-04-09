3d37fcb2571f8308f2fc8f9753e38cf1a3e11c27
Raghavendra Chari <56157111+hjrc-ct@users.noreply.github.com>
2021-04-09 23:57:26 +0530

    done

--- push.js
+++ push.js
@@ -4,7 +4,7 @@ let vapidKeys =
         //push.generateVAPIDKeys();
         {
             publicKey: 'BBlw0TujfU6PVbweYIULgv5nLRcwOhvgM5fjdzeLWEXqjHsKvshTk10Q7VFsS9G29y-dovhm5bwz3Vwh5k0tRNI',
-            privateKey: 'JY8cIeauPUtdafWBU8Y-TJuqUulZBUwxv64jqJFd2Us' 
+            privateKey: 'secret'
         };
 
 //console.log("VAPID keys for BPA -> ", vapidKeys)
@@ -15,14 +15,15 @@ push.setVapidDetails('mailto:security@ct.com',
 
 let sub = 
 {
-    "endpoint":"https://fcm.googleapis.com/fcm/send/cLVdJ4vT410:APA91bGBS1HF_hpBdFDY2kh7UvB_NYzRvJQvDpobF3q_aPjUMaNdsOlT2IV3ApfewgLQSlFRfA_d1RRXf8p4g0x8I8goxzEz5Qinrlh2XUTYjO7ZUdGzrv794nvoV-rOK-lAdmhYPZz8",
+    "endpoint":"https://fcm.googleapis.com/fcm/send/d6EhN9o_ykY:APA91bHuZnseIjK8N_6LBNFF3J_Lgh3wGrgiyy19Ydy49bwQpN9SqjBdbJ00xQ8NIgp7sZop1qIc-pJc27JMOhtgJoekkQJ344afgjNAP9YBbc2WmZdLvh1UGcLU2HOyo8rNJ1c0fkRs",
     "expirationTime":null,
-    
     "keys":{
-        "p256dh":"BNUaOX3IzIY84BD8ebtwSJHRkHxwMzKH9Nt7Ha8m1vfcwqvif5gydd8v3t_M_qe80vfbUGFkFCpu8fKv0faL9dI",
-        "auth":"uaypOjuqVoX2GXaJ0EwIww"
+        "p256dh":"BMuo1ocprsO8cvdl3fc_Pt2yd7cUptBNdSoL-8_FLC6qBcDpd05kVCygM3scjWdXYLp0bqxpUYHabJaaabJBEEU",
+        "auth":"TsxAJ3Mkjbv3wDNEYByqzA"
     }
 };
 
 push.sendNotification(sub, 'BPA Message One');
+
+
 
