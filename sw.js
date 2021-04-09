4ae6ba9b9c8dfc3362e53fb61631e907f6bc5d9a
Raghavendra Chari <56157111+hjrc-ct@users.noreply.github.com>
2021-04-09 23:14:10 +0530

    minor enh

--- sw.js
+++ sw.js
@@ -1,7 +1,27 @@
 // service worker
 
-self.addEventListener('push', () => {
+self.addEventListener('push', function( e ) {
     console.log('service worker registration ...');
-    self.register.sendNotification('BPA message', {} );
+    
+    var options = {
+        body: 'This notification was generated from a BPA push!',
+        icon: 'images/example.png',
+        vibrate: [100, 50, 100],
+        data : {
+            dateOfArrival: Date.now(),
+            primaryKey: '2'
+        },
+        actions : [
+            {
+                action: 'explore',
+                title: 'Explore this new BPA world',
+                icon: 'images/checkmark.png'
+            },
+            { action: 'close', title: 'Close', icon: 'images/xmark.png' }
+        ]
+    };
+
+    e.waitUntil( self.ServiceWorkerRegistration.showNotification('Hello BPA!', options));
+
 });
 
