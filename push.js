c3183901f39e1625c1e0f0956b81052d58ba80d5
Raghavendra Chari <raghavendra.chari@chainthat.com>
2021-04-16 10:54:56 +0530

    sw log added

--- push.js
+++ push.js
@@ -1,10 +1,11 @@
 var push = require('web-push')
 
+
 let vapidKeys = 
         //push.generateVAPIDKeys();
         {
             publicKey: 'BBlw0TujfU6PVbweYIULgv5nLRcwOhvgM5fjdzeLWEXqjHsKvshTk10Q7VFsS9G29y-dovhm5bwz3Vwh5k0tRNI',
-            privateKey: 'secret'
+            privateKey: 'JY8cIeauPUtdafWBU8Y-TJuqUulZBUwxv64jqJFd2Us'
         };  // keep privateKey in a secure location. Nobody should see the private key per se.  DevOps action.
 
 //console.log("VAPID keys for BPA -> ", vapidKeys)
@@ -17,21 +18,12 @@ push.setVapidDetails('mailto:security@ct.com',
 // user device endpoint registered
 let subDeviceList = [ 
     {
-        "endpoint":"https://fcm.googleapis.com/fcm/send/ezJJmtBJzsY:APA91bG7jcMR4zn5wXUw48RnkijyjlrfGJ_DsXOG5-n-DO2TYpHHJdhUoMwrc52nVCQORBUC9tPGQbIxqVQsgt-rdCjlxNO6ON3KoRAsh2gfPwnCwq8kQ0PECiWg7XVhwG6PjbPUj62P",
-        "expirationTime":null,
+        "endpoint":"https://updates.push.services.mozilla.com/wpush/v2/gAAAAABgeRc1oPN0at3neciycDW2fc-5JQQrL_NLz-1ujUk8aEsueFdG9wjHAV6z0EM5_UHh0xMlfKCRtZYaIApoFR2mb25PPYys25liZVpesdiHua9Kx0ApwPzWfbGQ8XKeASkmwJGt5RLMC01FEPm3rpmy9-sBq-hMsm3bofKLyAL3Y6f5vVc",
         "keys":{
-            "p256dh":"BECjEl1ZGzfVih6Sn2SIIp7uje0d-IMRLCLp-8rL3kby4jCOgyT7UCKoRKDyY14nWrRjNLPcNwSKQCbgq2Hsgno",
-            "auth":"v3mqZv70y4YtuvKE3KOnQw"
-        }
-    },
-    {
-        "endpoint":"https://updates.push.services.mozilla.com/wpush/v2/gAAAAABgd-lOcmcZEfEh_lnFe4Lu2biSiGsKEqvnsZO4X6LvAlrAoh10TBxs--mkVX6wTyQXUQDXMQwcc2wACk13__d1H75mmNckmKPvSG6g_ykWJvlSoQjtduGkTRHl6Im9QOmdOAVUjXNVu8U5rSHxuJjSgSXI695Fl97tPX6hVsaKES0KbWg",
-        "keys":
-            {
-                "auth":"SRzqoYiDbCa_fGkdUlm4hA","p256dh":"BJSD8vCnobOXqKeVMT8hHMf03y-PxwmR5Vf0fdb6JCg-JYkgRGaEakDEaM8oU3xqmW2b6NWsCbUcRxZAWCcUk2A"
-    
+            "auth":"vcHZ_ZSUKN1OSExSoIceCg","p256dh":"BOWYIvyQ7huxYN5djqIp2U2zbEVCpfKPhBqVwFPmyYhO6H6J5Q-SOczluYif_UCBahWbFF5faHw5ZkOQYNkuBtc"
         }
     }
+
 ];
 var msgCustomised = {
     "message"  : "BPA Message Eight 2",
